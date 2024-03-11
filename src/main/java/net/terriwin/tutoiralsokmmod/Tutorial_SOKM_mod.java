b6119926c518184ce4676c90f965333c74247d77
TERRIWIN <nikitos10b@gmail.com>
2024-03-11 22:37:28 +0300

    добавил предмет без текстурки но с моделькой

--- src/main/java/net/terriwin/tutoiralsokmmod/Tutorial_SOKM_mod.java
+++ src/main/java/net/terriwin/tutoiralsokmmod/Tutorial_SOKM_mod.java
@@ -2,18 +2,18 @@ package net.terriwin.tutoiralsokmmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.terriwin.tutoiralsokmmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -28,6 +28,8 @@ public class Tutorial_SOKM_mod
     public Tutorial_SOKM_mod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -39,7 +41,9 @@ public class Tutorial_SOKM_mod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event){
-
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS){
+            event.accept(ModItems.bundlecake);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
