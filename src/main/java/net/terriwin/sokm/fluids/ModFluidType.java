9b6ab565126f73b50d21b7f663409b2a467e5e29
Terriwin <nikitos10b@gmail.com>
2024-04-11 15:13:50 +0300

    last for MrMisc29

--- src/main/java/net/terriwin/sokm/fluids/ModFluidType.java
+++ src/main/java/net/terriwin/sokm/fluids/ModFluidType.java
@@ -2,6 +2,7 @@ package net.terriwin.sokm.fluids;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
 import net.minecraftforge.common.SoundAction;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidType;
@@ -10,17 +11,20 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.terriwin.sokm.sokm;
 import org.joml.Vector3f;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 public class ModFluidType {
-    public static final ResourceLocation WATER_STILL_RL = new ResourceLocation(sokm.MOD_ID,"misc/glaze");
-    public static final ResourceLocation WATER_FLOWING_RL = new ResourceLocation(sokm.MOD_ID,"misc/glaze");
-    public static final ResourceLocation GLAZE_OVERLAY_RL = new ResourceLocation(sokm.MOD_ID,"misc/glaze");
+    public static final ResourceLocation WATER_STILL_RL = new ResourceLocation(sokm.MOD_ID,"block/glaze");
+    public static final ResourceLocation WATER_FLOWING_RL = new ResourceLocation(sokm.MOD_ID,"block/glazeanim");
+    public static final ResourceLocation GLAZE_OVERLAY_RL = new ResourceLocation(sokm.MOD_ID,"block/glaze");
 
     public static final DeferredRegister<FluidType> FLUID_TYPES =
             DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, sokm.MOD_ID);
 
     public static final RegistryObject<FluidType> GLAZE_TYPE = register("glaze_fluid",
-            FluidType.Properties.create().lightLevel(2).density(15).viscosity(5).sound(SoundAction.get("drink"),
+            FluidType.Properties.create().lightLevel(2).density(1400).viscosity(2000).canSwim(false).supportsBoating(true).sound(SoundAction.get("drink"),
                     SoundEvents.HONEY_DRINK));
 
 
@@ -29,6 +33,10 @@ public class ModFluidType {
         return FLUID_TYPES.register(name, () -> new BaseFluidType(WATER_STILL_RL, WATER_FLOWING_RL, GLAZE_OVERLAY_RL,
                 0xA1FDAAFD, new Vector3f(253f / 255f, 170f / 255f, 195f / 253f), properties));
     }
+
+
+
+
 
     public static void register(IEventBus eventBus) {
         FLUID_TYPES.register(eventBus);
